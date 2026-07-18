0a1b2c129c03042ba88e8407a853cb9f17c4dcbb
mramadan-cell <m.ramadan@ignteq.org>
2026-07-18 16:45:52 +0300

    feat: Update AuthRepository to return Either<AuthFailure, CurrentUser> and add dartz dependency

--- mobile/lib/features/auth/domain/repositories/auth_repository.dart
+++ mobile/lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,12 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:src/features/auth/domain/failures/auth_failure.dart';
+
 import '../Entities/Current_User.dart';
 
 abstract interface class AuthRepository {
-  Future<CurrentUser> login({
+  Future<Either<AuthFailure, CurrentUser>> login({
     required String userNameOrEmail,
     required String password,
   });
 
-  Future<CurrentUser> register({
+  Future<Either<AuthFailure, CurrentUser>> register({
     required String userName,
     required String email,
     required String password,
